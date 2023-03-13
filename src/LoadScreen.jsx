b942f04a69e68da7d219a345c911ad0d4202970a
soumia elyakni <selyakni@gmail.com>
2023-03-13 11:13:00 +0100

    start frontend

---
+++ src/LoadScreen.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const LoadScreen = () => {
+  return (
+    <div>LoadScreen</div>
+  )
+}
---
+++ src/NewsScreen.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const NewsScreen = () => {
+  return (
+    <div>NewsScreen</div>
+  )
+}
