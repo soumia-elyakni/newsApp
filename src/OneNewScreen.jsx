c11393648f990bf3a24751342094a92734ba002f
soumia elyakni <selyakni@gmail.com>
2023-03-15 14:50:07 +0100

    create OneNewScreen component

--- src/OneNewScreen.jsx
+++ src/OneNewScreen.jsx
@@ -1,7 +1,71 @@
-import React from 'react'
+import React from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import { useRoute } from '@react-navigation/native';
 
-export const OneNewScreen = () => {
+
+const OneNewScreen = () => {
+  const route = useRoute();
+  const { title, description, contenu, date, image_url } = route.params.item;
+
   return (
-    <div>OneNewScreen</div>
-  )
-}
+    <View style={styles.container}>
+      <Text style={styles.title}>{title}</Text>
+      {image_url && <Image source={{ uri: image_url }} style={styles.image} />}
+      <Text style={styles.publishedAt}>{date}</Text>
+      <View style={styles.card}>
+        <View style={styles.descriptionContainer}>
+          <Text style={styles.description}>{description}</Text>
+        </View>
+        <View style={styles.contentContainer}>
+          <Text style={styles.content}>{contenu}</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#333',
+    marginBottom: 10,
+  },
+  image: {
+    height: 200,
+    width: '100%',
+    marginBottom: 10,
+  },
+  publishedAt: {
+    fontSize: 16,
+    color: 'grey',
+    marginBottom: 10,
+    textAlign : 'right',
+  },
+  card: {
+    backgroundColor: '#f1f1f1',
+    borderRadius: 10,
+    padding: 20,
+  },
+  descriptionContainer: {
+    marginBottom: 20,
+  },
+  description: {
+    fontSize: 18,
+    color: '#333',
+  },
+  contentContainer: {
+    marginTop: 20,
+  },
+  content: {
+    fontSize: 16,
+    color: '#333',
+    lineHeight: 24,
+  },
+});
+
+export default OneNewScreen;
