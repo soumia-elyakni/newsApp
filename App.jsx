ae01884489a785e362d83bc95fe2c1a9f2b73dcf
soumia elyakni <selyakni@gmail.com>
2023-03-15 14:47:29 +0100

    change createstacknavigation to createnativestacknavigator

--- App.jsx
+++ App.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
 
 // Import des composants pour chaque route
 import LoadScreen from './src/LoadScreen';
 import NewsScreen from './src/NewsScreen';
 import OneNewScreen from './src/OneNewScreen';
 
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Load" component={LoadScreen} />
-        <Stack.Screen name="News" component={NewsScreen} />
-        <Stack.Screen name="TheNew" component={OneNewScreen} />
+        <Stack.Screen name="Load" component={LoadScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="News" component={NewsScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="TheNew" component={OneNewScreen} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
