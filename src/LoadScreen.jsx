884446fb5c92a9417dd89d69e6356d6053606160
soumia elyakni <selyakni@gmail.com>
2023-03-15 14:48:09 +0100

    create load screen component

--- src/LoadScreen.jsx
+++ src/LoadScreen.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { useNavigation } from '@react-navigation/native';
+import { StyleSheet, Text , View, StatusBar, Image } from 'react-native'
+import { TouchableOpacity } from 'react-native';
+import Img from "./assets/logo.jpg"
 
-export const LoadScreen = () => {
+
+const LoadScreen = () => {
+  const navigation = useNavigation()
+
   return (
-    <div>LoadScreen</div>
+    <View style={{flex:1, flexDirection:'column', justifyContent:'center', alignItems:'center', backgroundColor:'grey'}}>
+    <StatusBar barStyle="light-content" hidden={false}  />
+    <Image  source={Img} />
+    <TouchableOpacity onPress={()=>navigation.navigate("News")}>
+  <Text>See News</Text>
+</TouchableOpacity>
+
+    </View>
   )
 }
+
+export default LoadScreen
